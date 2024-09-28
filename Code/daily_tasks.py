b1c35ecce452eacb41e683105e14c3cf92fe0252
Reethika Buddi <128274359+Reethz30@users.noreply.github.com>
2024-09-28 13:55:53 +0530

    Contains details about daily tasks that user have to follow

--- Code/daily_tasks.py
+++ Code/daily_tasks.py
@@ -120,7 +120,10 @@ def daily_tasks():
             #st.write(phobia_type,phobia_level)#precautions_df['level'],precautions_df['phobia'])
 
             # Filter the DataFrame based on user's phobia type and level
-            st.write(precautions_df,precautions_df[precautions_df['phobia'] == phobia_type])
+            #st.write(precautions_df,precautions_df[precautions_df['phobia'] == phobia_type])
+            c=precautions_df
+            c=c[precautions_df['phobia'] == phobia_type]
+            st.write(c)
             filtered_precautions = precautions_df[
             (precautions_df['phobia'] == phobia_type) &
             (precautions_df['level'] == phobia_level)]

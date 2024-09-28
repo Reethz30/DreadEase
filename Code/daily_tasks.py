ac8cea02eed671458e4164b00918a36dd0135329
Reethika Buddi <128274359+Reethz30@users.noreply.github.com>
2024-09-28 15:11:23 +0530

    Contains details about daily tasks that user have to follow

--- Code/daily_tasks.py
+++ Code/daily_tasks.py
@@ -119,12 +119,10 @@ def daily_tasks():
 
             phobia_type = phobia_type.lower()
             phobia_level = phobia_level.lower()
-            precaution_df['phobia'] = precaution_df['phobia'].str.lower()
+            precaution_df['phobia'] = precaution_df['phobia'].str.strip().lower()
             precaution_df['level'] = precaution_df['level'].str.lower()
             #st.write(precaution_df,precaution_df['phobia'],precaution_df['level'])
-            f=''
-            for i in phobia_type:
-                f+=i
+            
             test_filter = precaution_df[precaution_df['phobia'] == f]
             st.write("Test Filter Results:", test_filter, phobia_type)
             precautions_df=precaution_df
